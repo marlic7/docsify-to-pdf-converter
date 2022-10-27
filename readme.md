c77e458dbb4ffcf2df884268ad93ce6d2f7aa9da
Mariusz Lichota <mariusz.lichota@bankbps.pl>
2022-10-27 07:58:55 +0200

    conf: pathToDocsifyEntryPoint

--- readme.md
+++ readme.md
@@ -17,6 +17,9 @@ Example `.docsifytopdfrc.js` content:
 
 ```js
 module.exports = {
+  pathToDocsifyEntryPoint: "docs",
+  pathToStatic: "temp-pdf", 
+  mainMdFilename: "main.md",  
   contents: [ "docs/_sidebar.md" ], // array of "table of contents" files path
   pathToPublic: "pdf/readme.pdf", // path where pdf will stored
   pdfOptions: "<options for puppeteer.pdf()>", // reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const util = require("util");
 const path = require("path");
 const rimraf = require("rimraf");
-const yesno = require("yesno");
-const findFreePort = require('find-free-port');
 require("colors");
 
 const logger = require("./logger.js");
@@ -25,8 +23,8 @@ const safetyMkdir = async rawPath => {
 const removeArtifacts = async paths =>
   Promise.all(paths.map(path => new Promise(resolve => rimraf(path, resolve))));
 
-const prepareEnv = ({ pathToStatic, pathToPublic }) => () => {
-  const pathToStaticDir = path.resolve(pathToStatic);
+const prepareEnv = ({ pathToStatic, pathToPublic, pathToDocsifyEntryPoint }) => () => {
+  const pathToStaticDir = path.resolve(pathToDocsifyEntryPoint, pathToStatic);
   const pathToPublicDir = path.dirname(path.resolve(pathToPublic));
 
   return Promise.all([safetyMkdir(pathToStaticDir), safetyMkdir(pathToPublicDir)]).catch(err => {
@@ -34,31 +32,19 @@ const prepareEnv = ({ pathToStatic, pathToPublic }) => () => {
   });
 };
 
-const cleanUp = ({ pathToStatic, pathToPublic, removeTemp }) => async () => {
-  const isExist = await exists(path.resolve(pathToStatic));
+const cleanUp = ({ pathToStatic, pathToPublic, removeTemp, pathToDocsifyEntryPoint }) => async () => {
+  const isExist = await exists(path.resolve(pathToDocsifyEntryPoint, pathToStatic));
 
   if (!isExist) {
     return Promise.resolve();
   }
 
-  const questionStatic = `Path "${path.resolve(
-    pathToStatic,
-  )}" reserved for statics is already exists.${
-    removeTemp ? " It will be deleted." : ""
-  } Continue evaluating? (y/n)`.yellow;
-
-  const answer = await yesno.askAsync(questionStatic);
-
-  if (answer) {
-    return removeArtifacts([path.resolve(pathToPublic)]);
-  } else {
-    return Promise.reject("User stops evaluating");
-  }
+  return removeArtifacts([path.resolve(pathToDocsifyEntryPoint, pathToStatic)]);
 };
 
-const closeProcess = ({ pathToStatic, removeTemp }) => async code => {
+const closeProcess = ({ pathToStatic, removeTemp, pathToDocsifyEntryPoint }) => async code => {
   if (removeTemp) {
-    await removeArtifacts([path.resolve(pathToStatic)]);
+    await removeArtifacts([path.resolve(pathToDocsifyEntryPoint,pathToStatic)]);
   }
 
   return process.exit(code);
