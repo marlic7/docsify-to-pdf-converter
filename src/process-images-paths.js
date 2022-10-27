457677553e471dd9835d5ce0015fba2ad9474db6
Mariusz Lichota <mariusz.lichota@bankbps.pl>
2022-10-27 08:04:47 +0200

    fix: prevent random error when processing images

--- src/process-images-paths.js
+++ src/process-images-paths.js
@@ -16,13 +16,16 @@ module.exports = ({ pathToStatic, pathToDocsifyEntryPoint }) => ({ content, name
   markdownLinkExtractor(content)
     .filter(link => !isUrl(link))
     .filter(isImg)
-    .map(link => ({ origin: link, processed: path.resolve(dir, link) }))
+    .map(link => ({
+      origin: link,
+      processed: path.resolve(dir, link)
+    }))
     .map(({ origin, processed }) => ({
       origin,
       processed: path.relative(dirWithStatic, processed),
     }))
     .forEach(({ origin, processed }) => {
-      markdown = markdown.replace(origin, processed);
+      markdown = markdown.replace(`(${origin}`, `(${processed}`);
     });
 
   return markdown;
